1e49d543395e6e17cd0a43def365621857bd21d6
MiguelNiblock <miguelniblock@posteo.net>
2020-07-09 23:46:15 -0700

    auto-generated portfolio entries and style changes

---
+++ assets/js/portfolioData.js
@@ -0,0 +1,37 @@
+const PORTFOLIO = {
+	"Web Development" : [
+        {title: "COVID19 Preparedness Quiz", 
+        descr: "Single-page client-side app built on Javascript, HTML and CSS that implements a multiple-choice quiz meant to test your COVID preparedness, and give you answers to important issues on the subject.",
+        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png"},
+
+        {title: "Professional Portfolio Site", 
+        descr: "An appealing site which auto-generates its HTML content with JQuery selectors from a JS object. Refers to the current site. ",
+        thumbnail: "images/portfoliosite_screenshot.png"},
+
+        {title: "Greg's List- Results Page", 
+        descr: "Templateless HTML/CSS design which resembles the CraigsList look. Inclues a header with navbar, search area, pagination buttons/section, and results shown as rows.",
+        thumbnail: "images/gregslist_screenshot.png"},
+
+        {title: "Simple Sign-Up Page", 
+        descr: "A basic sign-up page for a fictitious company, using only HTML/CSS, with no templates.",
+        thumbnail: "images/signuppage_screenshot.png"},
+
+        {title: "Space Cat-Dog Layout", 
+        descr: "As you zoom in and out, this layout will maintain the placement of a funny cat and a dog at opposite corners of a content area.",
+        thumbnail: "images/spacecatdog_screenshot.png"}	
+    ],
+	"Data Science" : [
+		{title: "COVID19 Preparedness Quiz", 
+        descr: "Single-page client-side app built on Javascript, HTML and CSS that implements a multiple-choice quiz meant to test your COVID preparedness, and give you answers to important issues on the subject.",
+        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png"},
+        {title: "COVID19 Preparedness Quiz", 
+        descr: "Single-page client-side app built on Javascript, HTML and CSS that implements a multiple-choice quiz meant to test your COVID preparedness, and give you answers to important issues on the subject.",
+        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png"},
+        {title: "COVID19 Preparedness Quiz", 
+        descr: "Single-page client-side app built on Javascript, HTML and CSS that implements a multiple-choice quiz meant to test your COVID preparedness, and give you answers to important issues on the subject.",
+        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png"},
+        {title: "COVID19 Preparedness Quiz", 
+        descr: "Single-page client-side app built on Javascript, HTML and CSS that implements a multiple-choice quiz meant to test your COVID preparedness, and give you answers to important issues on the subject.",
+        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png"}
+    ]
+};
---
+++ assets/js/renderPortfolio.js
@@ -0,0 +1,45 @@
+function generatePortfolioString(dataObject){
+    console.log('generating porfolio section element')
+    const topicElements = Object.entries(dataObject).map((topic,index)=>{
+        const topicHeader = `
+        <header class="major">
+            <h3>${topic[0]}</h3>
+        </header>
+        `
+        const projects = topic[1].map((project,index)=>{
+        return `
+        <li>
+            <span class="image"><img src="${project["thumbnail"]}"></span>
+            <h4>${project["title"]}</h4>
+            <p>${project["descr"]}</p>
+        </li>
+        `
+        });
+
+        const joinedProjects = projects.join("");
+        // console.log(output);
+        const output = 
+            topicHeader +
+            "<ul class='features'>" + 
+            joinedProjects +
+            "</ul>"
+        return output
+    });
+
+    // console.log(topicElements);
+    joinedElements = topicElements.join("");
+    sectionElement = `
+        <header class="major">
+            <h2>Portfolio</h2>
+        </header>`+joinedElements
+    return sectionElement
+};
+
+function renderPortfolio(){
+    console.log('renderPortfolio ran');
+    const portfolioItemsString = generatePortfolioString(PORTFOLIO);
+    console.log(portfolioItemsString);
+    $('#portfolio').html(portfolioItemsString);
+};
+
+$(renderPortfolio);
