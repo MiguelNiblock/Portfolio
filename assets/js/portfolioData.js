8f625aeca0254ef1e02d5f43d2de1977740bda3a
MiguelNiblock <miguelniblock@posteo.net>
2020-07-11 11:05:49 -0700

    API project addition

--- assets/js/portfolioData.js
+++ assets/js/portfolioData.js
@@ -2,38 +2,37 @@ const PORTFOLIO = {
 	"Web App Development" : [
         {title: "COVID19 Preparedness Quiz", 
         descr: "Single-page client-side app built on Javascript, HTML and CSS that implements a multiple-choice quiz meant to test your COVID preparedness, and give you answers to important issues on the subject.",
-        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png"},
+        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png",
+        codeurl: "https://github.com/MiguelNiblock/COVID19_quiz"},
 
         {title: "Professional Portfolio Site", 
         descr: "An appealing site which auto-generates its HTML content with JQuery selectors from a JS object. Refers to the current site. ",
-        thumbnail: "images/portfoliosite_screenshot.png"},
+        thumbnail: "images/portfoliosite_screenshot.png",
+        codeurl: "https://github.com/MiguelNiblock/miguelniblock.github.io"},
     ],
-    "API Development" : [],
+    "API Development" : [
+        {title: "Blog API- Node.JS", 
+        descr: "CRUD API for blogging platform. Built from scratch with `npm` and Express. Handles GET and POST requests to `/blog-posts`. DELETE and PUT requests go to `/blog-posts/:id`. ",
+        thumbnail: "images/blogapi_screenshot.png",
+        codeurl: "https://github.com/MiguelNiblock/ServerDev/tree/master/CRUD/blog_app"},
+    ],
     "Web Design" : [
         {title: "Greg's List- Results Page", 
         descr: "Templateless HTML/CSS design which resembles the CraigsList look. Inclues a header with navbar, search area, pagination buttons/section, and results shown as rows.",
-        thumbnail: "images/gregslist_screenshot.png"},
+        thumbnail: "images/gregslist_screenshot.png",
+        codeurl: "https://github.com/MiguelNiblock/WebDev/tree/master/CSS-Layouts/Project-GregsList"},
 
         {title: "Simple Sign-Up Page", 
         descr: "A basic sign-up page for a fictitious company, using only HTML/CSS, with no templates.",
-        thumbnail: "images/signuppage_screenshot.png"},
+        thumbnail: "images/signuppage_screenshot.png",
+        codeurl: "https://github.com/MiguelNiblock/WebDev/tree/master/HTML-Forms/Project-SignUp-Page"},
 
         {title: "Space Cat-Dog Layout", 
         descr: "As you zoom in and out, this layout will maintain the placement of a funny cat and a dog at opposite corners of a content area.",
-        thumbnail: "images/spacecatdog_screenshot.png"}	
+        thumbnail: "images/spacecatdog_screenshot.png",
+        codeurl: "https://github.com/MiguelNiblock/WebDev/tree/master/CSS-Layouts/Project-Space-Cat-Dog"}	
     ],
 	"Data Science" : [
-		{title: "COVID19 Preparedness Quiz", 
-        descr: "Single-page client-side app built on Javascript, HTML and CSS that implements a multiple-choice quiz meant to test your COVID preparedness, and give you answers to important issues on the subject.",
-        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png"},
-        {title: "COVID19 Preparedness Quiz", 
-        descr: "Single-page client-side app built on Javascript, HTML and CSS that implements a multiple-choice quiz meant to test your COVID preparedness, and give you answers to important issues on the subject.",
-        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png"},
-        {title: "COVID19 Preparedness Quiz", 
-        descr: "Single-page client-side app built on Javascript, HTML and CSS that implements a multiple-choice quiz meant to test your COVID preparedness, and give you answers to important issues on the subject.",
-        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png"},
-        {title: "COVID19 Preparedness Quiz", 
-        descr: "Single-page client-side app built on Javascript, HTML and CSS that implements a multiple-choice quiz meant to test your COVID preparedness, and give you answers to important issues on the subject.",
-        thumbnail: "https://raw.githubusercontent.com/MiguelNiblock/COVID19_quiz/master/covid19quiz_screenshot.png"}
+		
     ]
 };
