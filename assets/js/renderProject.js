a714e40d4861287145dbf2e60d7aca6a18fcea7d
MiguelNiblock <miguelniblock@posteo.net>
2020-07-17 19:18:47 -0700

    completed markdown to html project regex patterns

--- assets/js/renderProject.js
+++ assets/js/renderProject.js
@@ -5,14 +5,65 @@ function renderProject(){
     
     console.log("projectId: "+ projectId);
 
-    $.get('../assets/js/Projects.js',function(data){
+    // $.get('../assets/js/Projects.js',function(data){
 
-        project = PROJECTS[projectId];
-        console.log(project);
-        $('h1').text(project["h1Title"]);
-        $('header.project p').text(project["headerP"])
-    },
-    'script')
+    //     project = PROJECTS[projectId];
+    //     console.log(project);
+    //     $('h1').text(project["h1Title"]);
+    //     $('header.project p').text(project["headerP"])
+    // },
+    // 'script')
+
+    $.get(`${projectId}.md`,function(data){
+        // data is the raw markdown text
+        console.log(data);
+
+        // separate md into content and head
+        contentRe = /#\s/g;
+        var posContent = data.search(contentRe);
+        // console.log(posContent);
+        var content = data.slice(posContent);
+        // console.log(content);
+        var head = data.split(contentRe)[0];
+        // console.log(head);
+
+        // the head from md goes in the DOM head
+        $('head').append(head);
+
+        // convert the md content into html
+        converter = new showdown.Converter();
+        var html = converter.makeHtml(content);
+
+        // get the h1 tag from the html
+        var h1Re = /<h1[\s\S]*<\/h1>/g
+        var h1Tag = html.match(h1Re)[0];
+        // console.log(h1Tag)
+        
+        // get the <p> after the h1
+        var h1pRe = /<\/h1>\s*<p>.*<\/p>/g
+        var h1p = html.match(h1pRe)[0];
+        // console.log(h1p)
+        var pRe = /<p>.*<\/p>/g
+        var headerP = h1p.match(pRe);
+
+        // add h1 and p into the header innerHTML
+        $('header').html(h1Tag+headerP);
+
+        // remove the h1Tag and headerP from html string. so we're left with only the main content
+        html = html.replace(h1Tag,"").replace(headerP,"")
+
+        // remove <p>'s surrounding <span>'s
+        var parspanRe = /<p><span.*<\/p>/g
+        var removePar = function(string){return string.replace('<p>','').replace('</p>','')}
+        html = html.replace(parspanRe,removePar)
+
+        // remove <p>'s surrounding <blockquotes>
+        var quoteparRe = /<blockquote>[\s\S]*<p>[\s\S]*<\/blockquote>/g
+        html = html.replace(quoteparRe,removePar)
+
+        console.log(html);
+        $('section#content').html(html);
+    },'text')
 };
 
 $(renderProject());
---
+++ projects/dsensemble.md
@@ -0,0 +1,40 @@
+    <title>DSEnsemble | Forecasting Algorithm</title>
+    <meta name="description" property="og:description" content="DSEnsemble is a forecasting algorithm for day series data with seasonality.">
+    <meta name="image" property="og:image" content="">
+    <meta name="author" content="Miguel Niblock">
+    <meta name="title" property="og:title" content="">
+
+# DSEnsemble
+
+Ensemble algorithm for forecasting day series data with seasonality.
+
+<span class="image main"><img src="../images/pic04.jpg" alt="" /></span>
+
+> <u>Note:</u> This project's source code is proprietary. The current page is a high-level description for informational purposes. A sample of the source code can be requested to Gaming Analytics, Inc. Please contact me for details.
+
+## Context
+
+During my time as an intern at Gaming Analytics, one of the obstacles faced by the team was the sometimes unimpressive accuracy of the forecasting model in production. Apparently it had been developed by a previous employee whose approach was one-of-a-kind and cryptic, and thus not worth trying to patch. So my task was to develop a forecasting module from scratch, that could improve accuracy and be more maintainable.
+
+After much experimentation and input from colleagues, I arrived at a novel and relatable approach that would combine two great algorithms: Holt Winters, and Facebook's Prophet.
+
+```python
+import pandas as pd
+string = "string"
+num = 1234
+```
+
+- list
+- items
+- here
+
+---
+
+1. ordered
+2. list
+3. items
+
+hey **bold** *italics* 
+
+
+
